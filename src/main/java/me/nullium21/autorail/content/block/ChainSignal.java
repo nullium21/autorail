868a95d42b05b9691d728dd32d726c3365c4addc
nullium21 <rommyskovets@gmail.com>
2021-07-29 15:05:25 +0300

    (Placeholder) Chain signal block

---
+++ src/main/java/me/nullium21/autorail/content/block/ChainSignal.java
@@ -0,0 +1,11 @@
+package me.nullium21.autorail.content.block;
+
+import net.minecraft.block.Block;
+
+public class ChainSignal extends Block {
+    public static final String NAME = "chain_signal";
+
+    public ChainSignal(Settings settings) {
+        super(settings);
+    }
+}
