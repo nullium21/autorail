3c00507a7c6e947166f7ece1e62639c58d56a78c
nullium21 <rommyskovets@gmail.com>
2021-07-29 15:35:05 +0300

    Item registration for blocks

--- src/main/java/me/nullium21/autorail/mod/registry/ARBlocks.java
+++ src/main/java/me/nullium21/autorail/mod/registry/ARBlocks.java
@@ -4,8 +4,11 @@ import me.nullium21.autorail.content.block.ARBlock;
 import me.nullium21.autorail.content.block.ChainSignal;
 import me.nullium21.autorail.content.block.Signal;
 import me.nullium21.autorail.mod.Autorail;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -25,9 +28,20 @@ public class ARBlocks implements ARRegistry {
             B_SIGNAL, B_CHAIN_SIGNAL
     );
 
+    public static final List<String> NO_ITEMS = List.of(
+
+    );
+
     @Override
     public void register() {
-        BLOCKS.forEach(block ->
-                Registry.register(Registry.BLOCK, new Identifier(Autorail.MODID, block.getIdentifier()), block));
+        BLOCKS.forEach(block -> {
+            Identifier id = new Identifier(Autorail.MODID, block.getIdentifier());
+
+            Registry.register(Registry.BLOCK, id, block);
+
+            if (!NO_ITEMS.contains(id.getPath()))
+                Registry.register(Registry.ITEM, id, new BlockItem(block, new FabricItemSettings()
+                                .group(ItemGroup.TRANSPORTATION)));
+        });
     }
 }
