8d3564ddb689ef7d3e9dad0c49c417ec4cc61637
nullium21 <rommyskovets@gmail.com>
2021-07-29 15:04:20 +0300

    (Placeholder) Signal block

---
+++ src/main/java/me/nullium21/autorail/content/block/Signal.java
@@ -0,0 +1,11 @@
+package me.nullium21.autorail.content.block;
+
+import net.minecraft.block.Block;
+
+public class Signal extends Block {
+    public static final String NAME = "rail_signal";
+
+    public Signal(Settings settings) {
+        super(settings);
+    }
+}
