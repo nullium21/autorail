d7e1c0267c36f271ee7e227b386099ddcd75b000
nullium21 <rommyskovets@gmail.com>
2021-07-30 14:12:54 +0300

    Make it not crash on load

--- src/main/java/me/nullium21/autorail/mod/AutorailClient.java
+++ src/main/java/me/nullium21/autorail/mod/AutorailClient.java
@@ -1,8 +1,14 @@
 package me.nullium21.autorail.mod;
 
+import net.fabricmc.api.ClientModInitializer;
+
 /**
  * Client-side entry point for the mod.
  */
 @net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
-public class AutorailClient {
+public class AutorailClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
 }
