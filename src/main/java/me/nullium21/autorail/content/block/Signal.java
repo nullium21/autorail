e6b4215f8e7619794039d819e2f6d12180ee5ab8
nullium21 <rommyskovets@gmail.com>
2021-07-30 14:45:31 +0300

    Misc fixes for signals

--- src/main/java/me/nullium21/autorail/content/block/Signal.java
+++ src/main/java/me/nullium21/autorail/content/block/Signal.java
@@ -2,8 +2,18 @@ package me.nullium21.autorail.content.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+import org.jetbrains.annotations.Nullable;
+
+import static net.minecraft.state.property.Properties.HORIZONTAL_FACING;
+import static net.minecraft.util.math.Direction.NORTH;
 
 /**
  * The rail signal block.
@@ -16,6 +26,9 @@ public class Signal extends ARBlock {
 
     public Signal(Settings settings) {
         super(settings);
+        setDefaultState(getStateManager().getDefaultState()
+                .with(CLOSED, false)
+                .with(HORIZONTAL_FACING, NORTH));
     }
 
     @Override
@@ -25,6 +38,19 @@ public class Signal extends ARBlock {
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
+        stateManager.add(HORIZONTAL_FACING);
         stateManager.add(CLOSED);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return getDefaultState()
+                .with(HORIZONTAL_FACING, ctx.getPlayerFacing().getOpposite());
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return VoxelShapes.cuboid(2/16d, 0d, 2/16d, 14/16d, 14/16d, 16/16d);
     }
 }
