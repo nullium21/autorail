43738dcfe85da4dc82f47062cb3529973163c22f
nullium21 <rommyskovets@gmail.com>
2021-08-01 22:01:51 +0300

    Try to fix double appearance of wrench chat messages

--- src/main/java/me/nullium21/autorail/content/item/Wrench.java
+++ src/main/java/me/nullium21/autorail/content/item/Wrench.java
@@ -8,6 +8,8 @@ import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.world.World;
 
+import static net.minecraft.util.Util.NIL_UUID;
+
 public class Wrench extends ARItem {
     public Wrench(Settings settings) {
         super(settings);
@@ -32,9 +34,11 @@ public class Wrench extends ARItem {
                     blockState.with(Signal.REDSTONE_BEHAVIOR, nextBehavior));
 
             if (ctx.getPlayer() != null)
-                ctx.getPlayer().sendMessage(Text.of(
+                ctx.getPlayer().sendSystemMessage(Text.of(
                         String.format("Changed redstone behavior from %s to %s.", currentBehavior, nextBehavior)),
-                        false);
+                        NIL_UUID);
+
+            return ActionResult.SUCCESS;
         }
 
         return super.useOnBlock(ctx);
