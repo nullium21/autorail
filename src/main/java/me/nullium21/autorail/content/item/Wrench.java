fb7e91de6995d306d3bb5ece41e03dec0772fbd8
nullium21 <rommyskovets@gmail.com>
2021-08-01 20:59:02 +0300

    Make wrench cycle through redstone behaviors when used on signal

--- src/main/java/me/nullium21/autorail/content/item/Wrench.java
+++ src/main/java/me/nullium21/autorail/content/item/Wrench.java
@@ -1,5 +1,13 @@
 package me.nullium21.autorail.content.item;
 
+import me.nullium21.autorail.content.block.Signal;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.world.World;
+
 public class Wrench extends ARItem {
     public Wrench(Settings settings) {
         super(settings);
@@ -8,5 +16,33 @@ public class Wrench extends ARItem {
     @Override
     public String getIdentifier() {
         return "rail_wrench";
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext ctx) {
+        World world = ctx.getWorld();
+        BlockState blockState = world.getBlockState(ctx.getBlockPos());
+        Block block = blockState.getBlock();
+
+        if (block instanceof Signal) {
+            Signal.RedstoneBehavior currentBehavior = blockState.get(Signal.REDSTONE_BEHAVIOR);
+            Signal.RedstoneBehavior nextBehavior = nextSignalRedstoneBehavior(currentBehavior);
+
+            world.setBlockState(ctx.getBlockPos(),
+                    blockState.with(Signal.REDSTONE_BEHAVIOR, nextBehavior));
+
+            if (ctx.getPlayer() != null)
+                ctx.getPlayer().sendMessage(Text.of(
+                        String.format("Changed redstone behavior from %s to %s.", currentBehavior, nextBehavior)),
+                        false);
+        }
+
+        return super.useOnBlock(ctx);
+    }
+
+    private static Signal.RedstoneBehavior nextSignalRedstoneBehavior(Signal.RedstoneBehavior current) {
+        return Signal.RedstoneBehavior.values()[
+                (current.ordinal() + 1) %
+                Signal.RedstoneBehavior.values().length];
     }
 }
