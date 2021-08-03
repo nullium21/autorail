28913e4cd06e115c4d67a2f8e0bff1e37b81af2a
nullium21 <rommyskovets@gmail.com>
2021-08-03 11:20:39 +0300

    Add recipes for items

--- src/main/java/me/nullium21/autorail/base/ARItem.java
+++ src/main/java/me/nullium21/autorail/base/ARItem.java
@@ -1,10 +1,9 @@
 package me.nullium21.autorail.base;
 
 import me.nullium21.autorail.base.interfaces.ARIdentifiable;
-import me.nullium21.autorail.base.interfaces.ARWithRecipe;
 import net.minecraft.item.Item;
 
-public abstract class ARItem extends Item implements ARIdentifiable, ARWithRecipe {
+public abstract class ARItem extends Item implements ARIdentifiable {
     public ARItem(Settings settings) {
         super(settings);
     }
--- src/main/java/me/nullium21/autorail/base/ARRecipe.java
+++
@@ -1,4 +0,0 @@
-package me.nullium21.autorail.base;
-
-public class ARRecipe {
-}
